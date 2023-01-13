8e5214a62b2aca30390e64c806074ac94e09cf91
Jahid Walid <jahedsocial2257@gmail.com$ git config --global user.email jahedsocial2257@gmail.comgit config --global user.email jahedsocial2257@gmail.com>
2023-01-14 02:26:33 +0600

    cart js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 
-let inputIphone=document.getElementById('input-iphone');
-let macAdd=document.getElementById('mac-add');
-let iphoneAdd=document.getElementById('iphone-add');
-
-
-
-macAdd.addEventListener('click', function(){
-    let inputMac=parseInt(document.getElementById('input-mac').value);
-    let macPriceText=document.getElementById('mac-price-text').innerText;
-    let macPrice=parseInt(macPriceText);
-    console.log(macPrice+macPrice);
-    let macTotal=macPrice*inputMac;
-    document.getElementById('mac-price-text').innerText=macTotal;
+let macPriceText=document.getElementById('mac-price-text').innerText;
+function handleProductChange(product, isIncrease){
+    let productInput=document.getElementById('input-mac');
+    let productCount=parseInt(productInput.value);
+    console.log(productCount);
+    const newProductCount=productInput;
     
-})
+
+    if(isIncrease==true){
+        newProductCount.value=productCount+1;
+    }else if(isIncrease==false){
+        newProductCount.value=productCount-1;
+    }
+    
+}
