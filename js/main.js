63f6d405d27d9783ece72da65a0887aa641675ac
Jahid Walid <jahedsocial2257@gmail.com$ git config --global user.email jahedsocial2257@gmail.comgit config --global user.email jahedsocial2257@gmail.com>
2023-01-15 20:04:54 +0600

    product total with js 1st way

--- js/main.js
+++ js/main.js
@@ -15,5 +15,17 @@ function handleProductChange(product, isIncrease){
         productTotal=newProductCount*150;
     }
     document.getElementById(product+"-total").innerText=productTotal;
+   handleCheckout()
+}
+function handleCheckout(){
+    const laptopCount=document.getElementById('laptop-count').value;
+    const iphoneCount=document.getElementById('iphone-count').value;
+   const totalPrice=parseInt(laptopCount) * 245 + parseInt(iphoneCount) *150; 
+   console.log(totalPrice)
+   document.getElementById('sub-total').innerText=totalPrice;
+   const tax=Math.floor(totalPrice*0.1);
+   document.getElementById('tax').innerText=tax;
+   const grantedTotal=tax+totalPrice;
+   document.getElementById('granted-total').innerText=grantedTotal;
 
 }
